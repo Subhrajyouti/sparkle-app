b9173d6cdc1fd72b31524a00860c0a0b4632f498
subhrajyoutimahanta@gmail.com <subhrajyoutimahanta@gmail.com>
2026-04-20 02:20:26 +0530

    m4

--- android/app/src/main/java/app/lovable/a781537c1a1549c0b33081a33a63309d/LocationForegroundService.java
+++ android/app/src/main/java/app/lovable/a781537c1a1549c0b33081a33a63309d/LocationForegroundService.java
@@ -2,7 +2,13 @@ package app.lovable.a781537c1a1549c0b33081a33a63309d;
 
 import android.app.*;
 import android.content.*;
-import android.location.*;
+import android.location.Location;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Priority;
 import android.os.*;
 import android.util.Log;
 import androidx.core.app.NotificationCompat;

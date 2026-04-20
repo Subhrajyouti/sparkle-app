d019d82450e9169057fcb518fe35cd39f067a94b
subhrajyoutimahanta@gmail.com <subhrajyoutimahanta@gmail.com>
2026-04-20 14:03:28 +0530

    server block

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,7 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.39afa90892844e8e8447bbbc33d8c9a4',
   appName: 'Khanismita Delivery',
   webDir: 'dist',
-  server: {
-    url: 'https://delivery.khanismitarecipes.online',
-    cleartext: true,
-  },
+  
   android: {
     useLegacyBridge: true,
   },
